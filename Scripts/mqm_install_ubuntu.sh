78ed67bfbb6366b7821a52e5e86666d4a844c245
Kushalgopal <56104679+Kushalgopal@users.noreply.github.com>
2021-06-18 17:42:16 +0530

    Updated source

--- Scripts/mqm_install_ubuntu.sh
+++ Scripts/mqm_install_ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-script_location="https://raw.githubusercontent.com/Kushalgopal/AzureARM_M02/master/install_mqm.sh"
+script_location="https://raw.githubusercontent.com/Kushalgopal/AzureARM_M02/master/Scripts/install_mqm.sh"
 cd ~
 wget -qO- -O install_mqm.sh $script_location
 # curl https://raw.githubusercontent.com/Kushalgopal/AzureARM/master/install_mqm.sh > ~/install_mqm.sh
