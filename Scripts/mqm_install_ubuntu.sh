af56f702f5fee3f627d96e26bf743dfc1a8576db
Kushalgopal <56104679+Kushalgopal@users.noreply.github.com>
2021-06-19 14:07:30 +0530

    Updated mqm_install_ubuntu.sh

--- Scripts/mqm_install_ubuntu.sh
+++ Scripts/mqm_install_ubuntu.sh
@@ -6,4 +6,5 @@ mkdir /usr/local/ibm_mq && cd /usr/local/ibm_mq
 wget -qO- -O install_mqm.sh $script_location
 # curl https://raw.githubusercontent.com/Kushalgopal/AzureARM/master/install_mqm.sh > ~/install_mqm.sh
 chmod u+x install_mqm.sh
-./install_mqm.sh
+bash install_mqm.sh
+echo "IBM MQ Server has been installed successfully"
