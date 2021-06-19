3e941ac822b17f3442044bfaba95bb7d2214b274
Kushalgopal <56104679+Kushalgopal@users.noreply.github.com>
2021-06-19 13:57:44 +0530

    Update mqm_install_ubuntu.sh

--- Scripts/mqm_install_ubuntu.sh
+++ Scripts/mqm_install_ubuntu.sh
@@ -6,4 +6,4 @@ mkdir /usr/local/ibm_mq && cd /usr/local/ibm_mq
 wget -qO- -O install_mqm.sh $script_location
 # curl https://raw.githubusercontent.com/Kushalgopal/AzureARM/master/install_mqm.sh > ~/install_mqm.sh
 chmod u+x install_mqm.sh
-#./install_mqm.sh
+./install_mqm.sh
